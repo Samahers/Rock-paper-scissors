dbc102ef62294640571f39cfde1c75bb9bc33141
Samahers <fattanisamaher@gmail.com>
2026-06-19 12:44:45 +0300

    Write getHumanChoice function and edit variables
    edit the name of variables in the getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -10,21 +10,33 @@ console.log("hello world!!!")
 
 
 function getComputerChoice(){
-    let computer_value_String;
-    let computer_value = Math.floor(Math.random() * 3);
-    console.log(computer_value);
-    switch (computer_value){
+    let computer_choice_string;
+    let computer_choice = Math.floor(Math.random() * 3);
+    console.log(computer_choice);
+    switch (computer_choice){
         case 0 :
-            computer_value_String = "rock";
+            computer_choice_string = "rock";
             break;
         case 1 :
-            computer_value_String = "paper";
+            computer_choice_string = "paper";
             break;
         case 2 :
-            computer_value_String = "scissors"
+            computer_choice_string = "scissors"
             break;
     }
-    return computer_value_String;
+    return computer_choice_string;
 }
 
 //console.log(getComputerChoice());
+
+//2. create getHumanChoice function 
+//prompt human to enter their choice 
+//print what they wrote
+
+function getHumanChoice(){
+    let playerChoice = prompt(`WELCOME IN A GAME OF ROCK, PAPERS, SCISSORS
+type your move`);
+    return playerChoice;
+}
+
+//console.log(getHumanChoice());
