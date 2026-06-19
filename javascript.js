5ba1c02ebfb6229a0f55fbc6b792b50e3f8b2e62
Samahers <fattanisamaher@gmail.com>
2026-06-19 12:28:30 +0300

    Write getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -9,3 +9,22 @@ console.log("hello world!!!")
 // 0 is rock;  1 is paper; 2 is scissors;
 
 
+function getComputerChoice(){
+    let computer_value_String;
+    let computer_value = Math.floor(Math.random() * 3);
+    console.log(computer_value);
+    switch (computer_value){
+        case 0 :
+            computer_value_String = "rock";
+            break;
+        case 1 :
+            computer_value_String = "paper";
+            break;
+        case 2 :
+            computer_value_String = "scissors"
+            break;
+    }
+    return computer_value_String;
+}
+
+//console.log(getComputerChoice());
