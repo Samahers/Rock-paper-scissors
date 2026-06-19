28f96f45bcc180ac86ddb3ce8d578059e2e04b3f
Samahers <fattanisamaher@gmail.com>
2026-06-19 12:14:50 +0300

    Add comment and start the project

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@ console.log("hello world!!!")
 
 // requirements 
 // 1. function called getComputerChoice
-//it returns the computer value using Math.random
+//it returns the computer choice using Math.random
+// 0 is rock;  1 is paper; 2 is scissors;
 
 
